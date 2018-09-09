51171af8af3cbb52ae2878b38c80376b895ae914
Nicole D <nnardella@gmail.com>
2018-09-09 12:17:50 +0000

    automatically backed up by learn

---
+++ objects.js
@@ -0,0 +1,11 @@
+var playlist = new Object({
+  Slowdive: 'Alison',
+});
+
+
+
+function updatePlaylist(playlist, artistName, songTitle){
+playlist[" My Bloody Valentine"] = "Sometimes";
+playlist["Phil Ochs"] = "Here's to the State of Mississippi";
+return updatePlaylist
+}
